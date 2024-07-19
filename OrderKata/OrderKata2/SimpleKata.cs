dc47c2d585971ac4573a58cc331e1c1cccd71815
Toby Wicks <toby.wicks@commercevision.com.au>
2024-07-19 13:01:18 +1000

    Basic kata greeting

---
+++ OrderKata/OrderKata2/SimpleKata.cs
@@ -0,0 +1,39 @@
+﻿namespace OrderKata2
+{
+    public class SimpleKataRunner
+    {
+        public static void Run()
+        {
+            var kata = new SimpleKata();
+            var result = kata.Greeting("hello");
+
+            Console.WriteLine(result.Item1);
+        }
+    }
+
+    internal class SimpleKata
+    {
+        public (string, bool) Greeting(string name)
+        {
+            if (Modes.GetMode() == Mode.Mode1)
+            {
+                return ("Hello", true);
+            }
+            else
+            {
+                return ("Goodbye", false);
+            }
+        }
+    }
+
+    public enum Mode
+    {
+        Mode1,
+        Mode2
+    }
+
+    public class Modes
+    {
+        public static Mode GetMode() => Mode.Mode1;
+    }
+}
