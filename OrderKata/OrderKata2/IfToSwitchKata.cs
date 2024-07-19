b2d4978dbae62990e53c4d32115fcc4e7e58288e
Toby Wicks <toby.wicks@commercevision.com.au>
2024-07-19 12:42:55 +1000

    DEVOPS-703 IfToSwitchStatement

---
+++ OrderKata/OrderKata2/IfToSwitchKata.cs
@@ -0,0 +1,28 @@
+﻿namespace OrderKata2
+{
+    internal class IfToSwitchKata
+    {
+        public static string Greeting()
+        {
+            if (Modes.GetMode() == Mode.Mode1)
+            {
+                return "Hello";
+            }
+            else
+            {
+                return "Goodbye";
+            }
+        }
+    }
+
+    public enum Mode
+    {
+        Mode1,
+        Mode2
+    }
+
+    public class Modes
+    {
+        public static Mode GetMode() => Mode.Mode1;
+    }
+}
